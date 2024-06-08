0a290a3688175830bd2e3a9e92dac494734e07d3
Mohamed Rafik <71353030+BounaderMedRafik@users.noreply.github.com>
2024-06-08 03:06:34 +0100

    finished everything

--- app/add-information/page.tsx
+++ app/add-information/page.tsx
@@ -1,9 +1,46 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import React from "react";
+import { useUser } from "@clerk/nextjs";
+import React, { useState } from "react";
+import supabase from "../supabase/supabaseClient";
+import toast from "react-hot-toast";
+import { Loader } from "lucide-react";
 
 export default function page() {
+  const { user } = useUser();
+  const [name, setName] = useState(user?.fullName);
+  const [email, setEmail] = useState(user?.emailAddresses[0].emailAddress);
+  const [univ, setUniv] = useState("");
+  const [Citation, setCitation] = useState("");
+  const [grade, setGrade] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const insertInfo = async () => {
+    if (!name || !email || !univ || !Citation || !grade) {
+      setError(true);
+      setLoading(false);
+      return;
+    }
+    const { data, error } = await supabase.from("informations").insert({
+      Name: name,
+      Email: email,
+      University: univ,
+      Citation: Citation,
+      Grade: grade,
+    });
+
+    if (error) {
+      console.log(error);
+      toast.error("There is an error");
+    } else {
+      toast.success("You Have added information successfully");
+      setLoading(false);
+    }
+  };
+
   return (
     <div className=" min-h-[80vh]  flex  items-center justify-between">
       <div className="flex items-center md:flex-row justify-between mx-auto flex-col">
@@ -15,18 +52,30 @@ export default function page() {
             <div>
               <Label>nom et prénom</Label>
               <Input
+                //@ts-ignore
+                value={name}
+                onChange={(e) => setName(e.target.value)}
                 className="mt-2"
                 type="text"
-                placeholder="example jhon sins"
+                //@ts-ignore
+                placeholder={name}
               />
             </div>
             <div className="mt-4">
               <Label>E-mail</Label>
-              <Input className="mt-2" type="text" placeholder="xyz@mail.com" />
+              <Input
+                className="mt-2"
+                placeholder={email}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                type="text"
+              />
             </div>
             <div className="mt-4">
               <Label>Universite</Label>
               <Input
+                value={univ}
+                onChange={(e) => setUniv(e.target.value)}
                 className="mt-2"
                 type="text"
                 placeholder="Chadli Ben Djedid"
@@ -35,6 +84,8 @@ export default function page() {
             <div className="mt-4">
               <Label>Citation</Label>
               <Input
+                value={Citation}
+                onChange={(e) => setCitation(e.target.value)}
                 className="mt-2"
                 type="text"
                 placeholder="Bibliography / Works"
@@ -42,10 +93,35 @@ export default function page() {
             </div>
             <div className="mt-4">
               <Label>Grade</Label>
-              <Input className="mt-2" type="text" placeholder="Master 2" />
+              <Input
+                className="mt-2"
+                value={grade}
+                onChange={(e) => setGrade(e.target.value)}
+                type="text"
+                placeholder="Master 2"
+              />
+            </div>
+            <div className="text-red-500 font-light mt-2">
+              {error ? "please fill out the form correctly" : ""}
             </div>
             <div className="mt-5">
-              <Button size={"lg"}>Ajouter information</Button>
+              <Button
+                disabled={loading}
+                onClick={() => {
+                  setError(false);
+                  setLoading(true);
+                  insertInfo();
+                }}
+                size={"lg"}
+              >
+                {loading ? (
+                  <div>
+                    <Loader className=" animate-spin" size={15} />
+                  </div>
+                ) : (
+                  <div>Ajouter information</div>
+                )}
+              </Button>
             </div>
           </div>
         </div>
