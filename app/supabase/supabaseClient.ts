716d732ed6adc1dfcb879e64930a21cc8ef39197
Mohamed Rafik <71353030+BounaderMedRafik@users.noreply.github.com>
2024-06-08 19:45:19 +0100

    kmjhnf

--- app/supabase/supabaseClient.ts
+++ app/supabase/supabaseClient.ts
@@ -1,8 +1,7 @@
-"use client";
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseURL = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseURL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 const supabase = createClient(supabaseURL, supabaseKEY);
 
